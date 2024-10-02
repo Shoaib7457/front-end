356684cde5793c8974a9bf4f21d30d4e01281910
shoaib Hussain <107783902+Shoaib7457@users.noreply.github.com>
2024-10-02 22:18:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
 import Map from './Map';
 
 function App() {
@@ -26,56 +24,11 @@ function App() {
 
 
 
-  //////
-
-    const [currentIndex, setCurrentIndex] = useState(null);
-
-  // Function to handle resetting currentIndex to 0 using fetch
-  const handleReset = async () => {
-    try {
-      const response = await fetch('https://back-end-86wm.onrender.com/reset-index', {
-        method: 'POST',
-      });
-      
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      setPolylineLatLngs([]);
-      const data = await response.json();
-      console.log(data.message); // Logs 'currentIndex reset to 0'
-      setCurrentIndex(data.currentIndex); // Update currentIndex in state
-    } catch (error) {
-      console.error('Error resetting currentIndex:', error);
-    }
-  };
-
-  // Function to handle refreshing currentIndex using fetch
-  const handleRefresh = async () => {
-    try {
-      const response = await fetch('https://back-end-86wm.onrender.com/current-index');
-      
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      setCurrentIndex(data.currentIndex); // Set the fetched currentIndex
-    } catch (error) {
-      console.error('Error fetching currentIndex:', error);
-    }
-  };
 
   return (
-    <div className="App">
     <div id='two'>
       <h1 id='one'>Vehicle Tracker with Leaflet and Node.js</h1>
       <Map vehiclePosition={vehiclePosition} routePath={routePath} />
-    </div>
-      {/* Adding new UI for Reset and Refresh */}
-      <br />
-      <button onClick={handleReset}>Reset Index</button>
-
-      {/* Other elements/components of your existing App.js */}
     </div>
   );
 }
